cbf45a91eaa99d81e8685f324dce3a7f777a933d
Cham <chiyamu.h.957@ms.saitama-u.ac.jp>
2021-07-15 18:13:00 -0400

    dev create callpeak windows version middle

--- R/callpeak.default.R
+++ R/callpeak.default.R
@@ -1,4 +1,6 @@
-callpeak.default<-function(outfolder,fragment,barcord_cluster_whole,oldRegFolder,macs2path,bedtoolspath){
+callpeak.default<-function(outfolder,fragment,barcord_cluster_whole,oldRegFolder,macs2path,bedtoolspath,cluster){
+
+line=length(c[,1])
 
 barcord_clusterF=paste0(outfolder,"barcord_cluster/")
 cmd=paste0("mkdir -p ",barcord_clusterF,";rm -f ",barcord_clusterF,"*")
@@ -13,18 +15,23 @@ cmd=paste0("mkdir -p ",peak_clusterF,";rm -f ",peak_clusterF,"*")
 system(command=cmd)
 
 
-cmd=paste0("line=`wc -l ", barcord_cluster_whole ,"|cut -f 1 -d ' ' `
-            echo $line
-           awk -v l=$line -v folder=",barcord_clusterF," \'{if(NR<=l){col[$1]=$2} else{if(length(col[$4])!=0){print >>folder col[$4]\".bed\"}}}\' ",barcord_cluster_whole," ",fragment)
+#cmd=paste0("line=`wc -l ", barcord_cluster_whole ,"|cut -f 1 -d ' ' `
+#            echo $line
+#           awk -v l=$line -v folder=",barcord_clusterF," \'{if(NR<=l){col[$1]=$2} else{if(length(col[$4])!=0){print >>folder col[$4]\".bed\"}}}\' ",barcord_cluster_whole," ",fragment)
+
+cmd=paste0("awk -v l=",line," -v folder=",barcord_clusterF," \'{if(NR<=l){col[$1]=$2} else{if(length(col[$4])!=0){print >>folder col[$4]\".bed\"}}}\' ",barcord_cluster_whole," ",fragment)
+
 system(command=cmd)
 
 
 
-cmd=paste0("for i in `ls ",barcord_clusterF,"`;
-           do i=`echo $i|tr -d \'.bed\'`;",
-           macs2path," callpeak -t ",barcord_clusterF,"$i.bed -g hs -f BED --nomodel --shift -100 --extsize 200 -n ",peak_clusterF,"$i;"
-           ,bedtoolspath," intersect -a ",peak_clusterF,"${i}_peaks.narrowPeak -b ",oldRegFolder,"Reg_cluster${i}.bed -wa -wb | cut -f 1,2,3,14,15 > ",RE_clusterF,"${i}.bed
-           done")
+#cmd=paste0("for i in `ls ",barcord_clusterF,"`;
+#           do i=`echo $i|tr -d \'.bed\'`;",
+#           macs2path," callpeak -t ",barcord_clusterF,"$i.bed -g hs -f BED --nomodel --shift -100 --extsize 200 -n ",peak_clusterF,"$i;"
+#           ,bedtoolspath," intersect -a ",peak_clusterF,"${i}_peaks.narrowPeak -b ",oldRegFolder,"Reg_cluster${i}.bed -wa -wb | cut -f 1,2,3,14,15 > ",RE_clusterF,"${i}.bed
+#           done")
+
+
 
 system(command=cmd)
 
