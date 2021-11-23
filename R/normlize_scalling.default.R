8e7912b43fed731140ee4311235569e2d679b93e
nachinGirid <64187405+nachinGirid@users.noreply.github.com>
2021-11-22 20:28:01 -0500

    Update normlize_scalling.default.R

--- R/normlize_scalling.default.R
+++ R/normlize_scalling.default.R
@@ -1,6 +1,14 @@
 normlize_scalling.default <- function (A){
-  B=as.matrix(A);
-  B[B==0]=NaN;
-  b=apply(B,2,median,na.rm=TRUE);
-  return(as(t(t(as.matrix(A))/b*mean(b)),"sparseMatrix"));
+ listCols<-function(m){
+    #converts a sparse Matrix into a list of its columns
+    res<-split(m@x, findInterval(seq_len(Matrix::nnzero(m)), m@p, left.open=TRUE))
+    names(res)<-colnames(m)
+    res
+}
+  list0=listCols(A)
+b=c()
+for (i in 1:length(list0)){
+b=c(b,median(list0[i][[1]],na.rm=TRUE))
+}
+return(Matrix::t(Matrix::t(E)/b*mean(b)))
 }
