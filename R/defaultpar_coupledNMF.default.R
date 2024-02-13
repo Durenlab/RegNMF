52b43e26f40934efeec01065f498b4362ea5e32f
Cham <43660308+cfg824157947@users.noreply.github.com>
2024-02-13 13:55:16 -0500

    Update defaultpar_coupledNMF.default.R
    add without RE version

--- R/defaultpar_coupledNMF.default.R
+++ R/defaultpar_coupledNMF.default.R
@@ -1,5 +1,9 @@
 defaultpar_CoupledNMF.default <- function(PeakO,W1,X,W2,Reg,W3,beta=1,arfa=1){
   lambda1=beta*mean(t(W1)%*%PeakO)/mean(t(W2)%*%X);
-  lambda2=arfa*mean(t(W1)%*%PeakO)/mean(t(W3)%*%Reg);
-  return(list(lambda1,lambda2));
+  if(Reg != FALSE)
+    lambda2=arfa*mean(t(W1)%*%PeakO)/mean(t(W3)%*%Reg);
+    return(list(lambda1,lambda2));
+  } else{
+    return(list(lambda1,0))
+  }
 }
